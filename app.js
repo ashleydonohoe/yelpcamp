ba26997e3f858c14334455b8924118f0d71d70cd
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-10-25 00:46:17 +0000

    added post route and ability to add new campground

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 var campgrounds = [
     {
         name: "Happy Creek", image: "https://static.pexels.com/photos/27865/pexels-photo-27865-large.jpg"
@@ -20,6 +24,21 @@ app.get("/", function(req, res) {
 
 app.get("/campgrounds", function(req, res) {
     res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", function(req, res) {
+    // get form data
+    var name = req.body.name;
+    var image = req.body.image;
+    // add to campgrounds array
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    // redirect back to campgrounds page
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res) {
+   res.render("new"); 
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
