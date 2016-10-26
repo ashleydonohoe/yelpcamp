28baf8668226d58a29591eab55554e59b6cfa9a7
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-10-26 02:45:51 +0000

    added mongoose and mongodb with ability to create campgrounds and show them

--- app.js
+++ app.js
@@ -1,58 +1,49 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
-
-var campgrounds = [
-    {
-        name: "Happy Creek", image: "https://static.pexels.com/photos/27865/pexels-photo-27865-large.jpg"
-    },
-    {
-        name: "Granite Creek", image: "https://static.pexels.com/photos/27679/pexels-photo-27679-large.jpg"
-    },
-    {
-        name: "Rainy Falls", image: "https://static.pexels.com/photos/25540/pexels-photo-25540.jpg"
-    },
-       {
-        name: "Happy Creek", image: "https://static.pexels.com/photos/27865/pexels-photo-27865-large.jpg"
-    },
-    {
-        name: "Granite Creek", image: "https://static.pexels.com/photos/27679/pexels-photo-27679-large.jpg"
-    },
-    {
-        name: "Rainy Falls", image: "https://static.pexels.com/photos/25540/pexels-photo-25540.jpg"
-    },
-       {
-        name: "Happy Creek", image: "https://static.pexels.com/photos/27865/pexels-photo-27865-large.jpg"
-    },
-    {
-        name: "Granite Creek", image: "https://static.pexels.com/photos/27679/pexels-photo-27679-large.jpg"
-    },
-        {
-        name: "Granite Creek", image: "https://static.pexels.com/photos/27679/pexels-photo-27679-large.jpg"
-    }
-    ];
-
 app.set("view engine", "ejs");
+
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema( {
+   name: String,
+   image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
 
 app.get("/", function(req, res) {
    res.render("landing"); 
 });
 
 app.get("/campgrounds", function(req, res) {
-    res.render("campgrounds", {campgrounds: campgrounds});
+    //Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds) {
+        if(err) {
+            console.log(err);
+        } else {
+            // Show campgrounds
+        res.render("campgrounds", {campgrounds: allCampgrounds});
+        }
+    });
 });
 
 app.post("/campgrounds", function(req, res) {
     // get form data
     var name = req.body.name;
     var image = req.body.image;
-    // add to campgrounds array
     var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    // redirect back to campgrounds page
-    res.redirect("/campgrounds");
+    Campground.create(newCampground, function(err, newCampground) {
+       if(err) {
+           console.log(err);
+       } else {
+            // redirect back to campgrounds page
+            res.redirect("/campgrounds");
+       }
+    });
 });
 
 app.get("/campgrounds/new", function(req, res) {
