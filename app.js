c6a224fe8157b93323eb2ffacdca205f7f9493ed
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-11-03 01:05:52 +0000

    fixed comment code

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var Campground = require("./models/campground");
+var Comment = require("./models/comment");
+var seedDB = require("./seeds");
 
-
+seedDB();
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
--- seeds.js
+++ seeds.js
@@ -1,10 +1,58 @@
 var mongoose = require('mongoose');
 var Campground = require("./models/campground");
+var Comment = require("./models/comment");
 
-Campground.remove({}, function(err) {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log("Removed campgrounds"); 
+var data = [
+    { 
+        name: "Cloud's Rest", 
+        image: "https://static.pexels.com/photos/30915/pexels-photo-30915-large.jpg", 
+        description: "blah blah blah"
+    }, 
+    { 
+        name: "Camp Cloudy", 
+        image: "https://static.pexels.com/photos/127634/pexels-photo-127634-large.jpeg", 
+        description: "blah blah blah"
+    }, 
+        { 
+        name: "Starry Falls", 
+        image: "https://static.pexels.com/photos/112378/pexels-photo-112378-large.jpeg", 
+        description: "blah blah blah"
     }
-});
+];
+
+function seedDB() {
+// Remove all campgrounds
+    Campground.remove({}, function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("Removed campgrounds");
+            // Add a few campgrounds
+         data.forEach(function(seed) {
+            Campground.create(seed, function(err, campground) {
+                if(err) {
+                    console.log(err);
+                } else {
+                    console.log("added campground");
+                    // Create a comment
+                    Comment.create(
+                        {
+                            text: "This place is great, but I wish there was internet",
+                            author: "Homer"
+                        }, function(err, comment){
+                            if(err){
+                                console.log(err);
+                            } else {
+                                campground.comments.push(comment);
+                                campground.save();
+                                console.log("Created new comment");
+                            }
+                        });
+                }
+            });
+        });
+        }
+    });
+} // End seedDB
+
+module.exports = seedDB;
